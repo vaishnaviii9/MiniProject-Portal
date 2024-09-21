99efd585acb7f8533db61e4458a9bc6f36aae7cf
vaishnaviii9 <vaishnavitonpe0@gmail.com>
2024-09-21 18:51:21 +0530

    Mini-Project Portal

---
+++ frontend/src/components/Button.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import clsx from "clsx";
+const Button = ({ icon, className, label, type, onClick = () => {} }) => {
+  return (
+    <button
+    type={type || "button" }
+    className={clsx("bx-3 py-2 outline-none", className)}
+    >
+      <span>{label}</span>
+      {icon && icon}
+    </button>
+  );
+};
+
+export default Button;
